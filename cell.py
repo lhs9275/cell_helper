9dcfe535be25a2e8cdf8b3dd98062d861bfd55ee
whoMI <98660600+lhs9275@users.noreply.github.com>
2024-01-19 10:29:46 +0900

    Update cell.py

--- cell.py
+++ cell.py
@@ -6,9 +6,10 @@ from openpyxl.styles import Font
 from datetime import datetime
 
 # 엑셀 파일
+print("안녕")
 
 change_colums = 0
-df_data = pd.read_excel('reservation.xlsx', index_col=0)
+df_data = pd.read_excel('예약.xlsx', index_col=0)
 new_column_names = ['1코트', '2코트', '3코트','4코트','5코트','6코트','7코트','8코트']  # 필요한 만큼 열 이름을 변경
 new_index_values = ['06:00~07:00', '07:00~08:00', '08:00~09:00', '09:00~10:00', '10:00~11:00', '11:00~12:00', '12:00~13:00','13:00~14:00','14:00~15:00','15:00~16:00','16:00~17:00','17:00~18:00','18:00~19:00','19:00~20:00','20:00~21:00','21:00~22:00'] #행
 
@@ -23,9 +24,6 @@ desired_reservation_time_list_4_r = ['6-10', '8-12', '10-14', '12-16', '14-18', 
 
 # 새로운 엑셀 파일을 생성
 df_sch = pd.DataFrame(index=new_index_values, columns=new_column_names)
-
-
-today_date = datetime.today().strftime('%m-%d')
 
 #조건문 
 
@@ -68,7 +66,7 @@ df_sch = df_sch.replace('nan', '')
 # ExcelWriter 객체 생성
 with pd.ExcelWriter('tennis_court_schedule.xlsx', engine='openpyxl') as writer:
     # DataFrame을 Excel 파일에 쓰기
-    df_sch.to_excel(writer, sheet_name='Sheet1', startcol=0, startrow=0, header=True, index=True)
+    df_sch.to_excel(writer, sheet_name='Sheet1', startcol=0, startrow=1, header=True, index=True)
 
     # ExcelWriter 객체에서 워크북과 워크시트 객체 가져오기
     workbook  = writer.book
@@ -76,16 +74,21 @@ with pd.ExcelWriter('tennis_court_schedule.xlsx', engine='openpyxl') as writer:
 
     # 열의 너비를 15로 설정
     for col_num, value in enumerate(new_column_names):
-        worksheet.column_dimensions[worksheet.cell(row=1, column=col_num+1).column_letter].width = 13
+        worksheet.column_dimensions[worksheet.cell(row=2, column=col_num+1).column_letter].width = 13
 
     # 높이 설정
     for row_num, value in enumerate(new_index_values):
-        worksheet.row_dimensions[row_num + 2].height = 25
+        worksheet.row_dimensions[row_num + 3].height = 25
 
+    # 헤더 텍스트 추가
+    header_text = "                            정구장 (클레이 코트)             월     일    요일"  # 페이지 번호를 나타내는 예시
+    worksheet['A1'] = header_text
+    worksheet.merge_cells('A1:I1')
 
-    worksheet['A1'] = today_date
+    # A1 셀의 높이를 늘리기
+    worksheet.row_dimensions[1].height = 30
 
-
+    worksheet['A1'].font = Font(size=16)
     # 외곽에 선 추가
     border = Border(left=Side(style='thin'), 
                     right=Side(style='thin'), 
@@ -102,14 +105,16 @@ with pd.ExcelWriter('tennis_court_schedule.xlsx', engine='openpyxl') as writer:
     for row in worksheet.iter_rows(min_row=1, max_row=worksheet.max_row, min_col=1, max_col=worksheet.max_column):
         for cell in row:
             cell.border = border
-            cell.alignment = Alignment(horizontal='center', vertical='center',wrap_text=True)
+            cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
             cell.font = Font(size=9)
 
     # 안쪽에 선 추가
     for row in worksheet.iter_rows(min_row=2, max_row=worksheet.max_row, min_col=1, max_col=worksheet.max_column):
         for cell in row:
             cell.border = inside_border
-            cell.alignment = Alignment(horizontal='center', vertical='center',wrap_text=True)
+            cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
             cell.font = Font(size=9)
+
+    worksheet['A1'].font = Font(size=16, bold=True)
 
 print("엑셀 파일이 생성되었습니다.")
